4b9bfa5be4c73d01157cff2d42e122f0ee382f18
mooktungs <mook13168@hotmail.com>
2016-02-20 22:40:52 +0000

    add random move

--- app/main.py
+++ app/main.py
@@ -36,7 +36,7 @@ def move():
     data = bottle.request.json
 
     # TODO: Do things with data
-    head[1] = data.coor[1]
+    head = data.coords[0]
     x = random.randint(1, 4)
     if x == 1:
         return {
